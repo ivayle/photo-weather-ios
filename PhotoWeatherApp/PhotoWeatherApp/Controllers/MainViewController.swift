5235525069f4aee9670b16e7ccc4e1e15245bc02
Ivaylo Petrov <ivaylo.petrov@wacom.com>
2023-11-20 20:32:55 +0200

    Set new UI constraints.

--- PhotoWeatherApp/PhotoWeatherApp/Controllers/MainViewController.swift
+++ PhotoWeatherApp/PhotoWeatherApp/Controllers/MainViewController.swift
@@ -6,16 +6,53 @@
 //
 
 import UIKit
+import Photos
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var geocoder: CLGeocoder?
+    var imagePicker: ImagePicker!
+    var imageDate: Date?
+    var imageLocation: CLLocation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        self.imagePicker = ImagePicker(presentationController: self, delegate: self)
+        
+        geocoder = CLGeocoder()
+        
+        let status = PHPhotoLibrary.authorizationStatus()
+
+        if status == .notDetermined  {
+            PHPhotoLibrary.requestAuthorization({status in
+            })
+        }
     }
     
-    @IBAction func tapOnSelectPhotoButton(_ sender: Any) {
-        
+    @IBAction func tapOnSelectPhotoButton(_ sender: UIView) {
+        self.imagePicker.present(from: sender)
     }
 }
 
+extension MainViewController: ImagePickerDelegate {
+    
+    func didSelect(image: UIImage?, info: [UIImagePickerController.InfoKey : Any]) {
+        self.imageView.image = image
+        
+        let asset = info[.phAsset] as? PHAsset
+        imageDate = asset?.creationDate
+        imageLocation = asset?.location
+        
+        print(asset?.creationDate ?? "None")
+        print(asset?.location ?? "None")
+        
+        if let location = self.imageLocation {
+            geocoder?.reverseGeocodeLocation(location) { placemarks, error in
+                print(placemarks?.first?.locality ?? "None")
+            }
+        }
+    }
+}
---
+++ PhotoWeatherApp/PhotoWeatherApp/Models/ImagePicker.swift
@@ -0,0 +1,93 @@
+//
+//  ImagePicker.swift
+//  PhotoWeatherApp
+//
+//  Created by Ivaylo Petrov on 20.11.23.
+//
+
+import Foundation
+import UIKit
+
+public protocol ImagePickerDelegate: AnyObject {
+    func didSelect(image: UIImage?, info: [UIImagePickerController.InfoKey: Any])
+}
+
+open class ImagePicker: NSObject {
+
+    private let pickerController: UIImagePickerController
+    private weak var presentationController: UIViewController?
+    private weak var delegate: ImagePickerDelegate?
+
+    public init(presentationController: UIViewController, delegate: ImagePickerDelegate) {
+        self.pickerController = UIImagePickerController()
+
+        super.init()
+
+        self.presentationController = presentationController
+        self.delegate = delegate
+
+        self.pickerController.delegate = self
+        self.pickerController.mediaTypes = ["public.image"]
+    }
+
+    private func action(for type: UIImagePickerController.SourceType, title: String) -> UIAlertAction? {
+        guard UIImagePickerController.isSourceTypeAvailable(type) else {
+            return nil
+        }
+
+        return UIAlertAction(title: title, style: .default) { [unowned self] _ in
+            self.pickerController.sourceType = type
+            self.presentationController?.present(self.pickerController, animated: true)
+        }
+    }
+
+    public func present(from sourceView: UIView) {
+
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+
+        if let action = self.action(for: .photoLibrary, title: "Photo Library") {
+            alertController.addAction(action)
+        }
+
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            alertController.popoverPresentationController?.sourceView = sourceView
+            alertController.popoverPresentationController?.sourceRect = sourceView.bounds
+            alertController.popoverPresentationController?.permittedArrowDirections = [.down, .up]
+        }
+
+        self.presentationController?.present(alertController, animated: true)
+    }
+    
+    private func pickerController(_ controller: UIImagePickerController) {
+        controller.dismiss(animated: true, completion: nil)
+
+        self.delegate?.didSelect(image: nil, info: [:])
+    }
+
+    private func pickerController(_ controller: UIImagePickerController, didSelect image: UIImage?, withInfo info: [UIImagePickerController.InfoKey: Any]) {
+        controller.dismiss(animated: true, completion: nil)
+
+        self.delegate?.didSelect(image: image, info: info)
+    }
+}
+
+extension ImagePicker: UIImagePickerControllerDelegate {
+
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.pickerController(picker)
+    }
+
+    public func imagePickerController(_ picker: UIImagePickerController,
+                                      didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        guard let image = info[.originalImage] as? UIImage else {
+            return self.pickerController(picker, didSelect: nil, withInfo: info)
+        }
+
+        self.pickerController(picker, didSelect: image, withInfo: info)
+    }
+}
+
+extension ImagePicker: UINavigationControllerDelegate {
+}
